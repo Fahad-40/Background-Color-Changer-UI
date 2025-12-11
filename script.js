971fb15cf3e797597f7f0c469217e6682f93988b
Fahad <fahad05munir@gmail.com>
2025-12-11 13:14:41 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function bgChange(colorIndex) {
+    const colorArray = [
+        "#CDB4DB", // soft lavender
+        "#BDE0FE", // mint sky
+        "#A3D5D3", // pastel aqua
+        "#FFC8DD", // peach mist
+        "#5DD39E"  // ocean teal
+    ];
+
+    document.body.style.background = colorArray[colorIndex];
+}
+
+const colors = ["#CDB4DB", "#BDE0FE", "#A3D5D3", "#FFC8DD", "#5DD39E"];
+
+const colorBox = document.querySelector("#colorBox");
+
+colors.forEach((color, index) => {
+    const circle = document.createElement("span");
+    circle.style.backgroundColor = color;
+
+    circle.addEventListener("click", () => bgChange(index));
+
+    colorBox.appendChild(circle);
+});
