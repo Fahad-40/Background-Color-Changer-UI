18f65857e270c8639c5964ebdb47aad79689feda
Fahad Muneer <fahad05munir@gmail.com>
2025-12-11 13:21:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function bgChange(colorIndex) {
     const colorArray = [
         "#CDB4DB", 
         "#BDE0FE", 
-        "#A3D5D3", // pastel aqua
+        "#A3D5D3", 
         "#FFC8DD", // peach mist
         "#5DD39E"  // ocean teal
     ];
